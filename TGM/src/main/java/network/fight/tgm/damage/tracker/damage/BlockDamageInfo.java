fc21b6a24d70b99db64341361393f89e9e626a73
Deterno <32507776+DeternoMC@users.noreply.github.com>
2017-10-25 13:48:34 +1100

    Create BlockDamageInfo.java

---
+++ TGM/src/main/java/network/fight/tgm/damage/tracker/damage/BlockDamageInfo.java
@@ -0,0 +1,19 @@
+package deterno.fight.tgm.damage.tracker.damage;
+
+import deterno.fight.tgm.damage.tracker.DamageInfo;
+import org.bukkit.block.BlockState;
+
+import javax.annotation.Nonnull;
+
+/**
+ * Represents a damage caused by a specific block in the world.
+ */
+public interface BlockDamageInfo extends DamageInfo {
+    /**
+     * Gets the world block responsible for this damage.
+     *
+     * @return Snapshot of the damaging block
+     */
+    @Nonnull
+    BlockState getBlockDamager();
+}
